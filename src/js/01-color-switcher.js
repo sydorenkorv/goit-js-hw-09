924755c5004dbe9e2de225857156c5b57a46e8c8
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2022-12-09 14:19:33 +0000

    done 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]')
+let timerId = null;
+stopBtn.disabled = "true"
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', function () {
+    stopBtn.removeAttribute('disabled');
+    startBtn.disabled = "true"
+  timerId = setInterval(() => {
+    const colorBg = getRandomHexColor();
+    document.body.style.background = colorBg;
+  }, 1000);
+    
+});
+
+stopBtn.addEventListener('click', function () {
+  clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+    stopBtn.disabled = "true"
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,4 @@
+// Described in documentation
+import flatpickr from "flatpickr";
+// Additional styles import
+import "flatpickr/dist/flatpickr.min.css";
