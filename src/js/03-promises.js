993f2ed23f7ade6aa846104aa0fb0db600437da2
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2022-12-12 11:36:45 +0000

    3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form')
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onCreatePromises(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  const dataParams = {};
+
+  for (const [key, value] of formData.entries()) {
+    dataParams[key] = Number(value);
+  }
+
+  let { amount, step, delay } = dataParams;
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay += step;
+    createPromise(i, delay).then(onSuccess).catch(onError);
+
+    form.reset();
   }
 }
+
+function onError({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
+function onSuccess({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+form.addEventListener('submit', onCreatePromises);
